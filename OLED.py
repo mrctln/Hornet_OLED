410a087370d0709ceec4d5e35a5e9669c1901881
mrc.tln <60466333+mrctln@users.noreply.github.com>
2020-02-05 17:44:20 +0100

    Update OLED.py

--- OLED.py
+++ OLED.py
@@ -14,6 +14,20 @@ def getCpuTemperature():
  tempFile.close()
  return float(cpu_temp)/1000
 
+def get_RAM_usage():
+  mem=str(os.popen('free -t -m').readlines())
+  T_ind=mem.index('T')
+  mem_G=mem[T_ind+14:-4]
+  S1_ind=mem_G.index(' ')
+  mem_T=mem_G[0:S1_ind]
+  mem_G1=mem_G[S1_ind+8:]
+  S2_ind=mem_G1.index(' ')
+  mem_U=mem_G1[0:S2_ind]
+  mem_F=mem_G1[S2_ind+8:]
+  k=(int((100/int(mem_G[:5]))*(int(mem_F[8:]))))
+  i=100-k
+  return i
+
 i2cbus = SMBus(1)            # 0 = Raspberry Pi 1, 1 = Raspberry Pi > 1
 oled = ssd1306(i2cbus)
 draw = oled.canvas
@@ -25,40 +39,26 @@ y=0
 j=0
 
 while True:
-    
-
-    mem=str(os.popen('free -t -m').readlines())
-    T_ind=mem.index('T')
-    mem_G=mem[T_ind+14:-4]
-    S1_ind=mem_G.index(' ')
-    mem_T=mem_G[0:S1_ind]
-    mem_G1=mem_G[S1_ind+8:]
-    S2_ind=mem_G1.index(' ')
-    mem_U=mem_G1[0:S2_ind]
-
-    mem_F=mem_G1[S2_ind+8:]
-    
-    x=int(getCpuTemperature())
-    k=(int((100/int(mem_G[:5]))*(int(mem_F[8:]))))
-    i=100-k
-    mem=str(os.popen('systemctl status hornet').readlines())
-    index=mem.index("Active")
-    status=mem[index+8:index+14]
-    if (status == "active"):
-            node_status="Online"
-    else:
-            node_status="Offline"
+ RAM_usage=get_RAM_usage()
+ CPU_temp=int(getCpuTemperature())
+ bash_status=str(os.popen('systemctl status hornet').readlines())
+ index=bash_status.index("Active")
+ status=bash_status[index+8:index+14]
+ if (status == "active"):
+         node_status="Online"
+ else:
+         node_status="Offline"
 
 
-    if x!=y or i!=j:
-        oled.cls()
-        draw.text((30, 10), "CPU: " + str(x)+"°C", font=DejaVuSans30,fill=1)
-        draw.text((30, 28), "RAM: " + str(i) + "%", font=DejaVuSans30,fill=1)
-        draw.text((30, 46), "Status: " + p, font=DejaVuSans30,fill=1)
+ if CPU_temp!=CPU_temp_before or RAM_usage!=RAM_usage_before:
+     oled.cls()
+     draw.text((30, 10), "CPU: " + str(CPU_temp)+"°C", font=DejaVuSans30,fill=1)
+     draw.text((30, 28), "RAM: " + str(RAM_usage) + "%", font=DejaVuSans30,fill=1)
+     draw.text((30, 46), "Status: " + node_status, font=DejaVuSans30,fill=1)
 
-        oled.display()
-        time.sleep(1)
-        y=x
-        j=i
-    else:
-        time.sleep(1)
+     oled.display()
+     time.sleep(1)
+     CPU_temp_before=CPU_temp
+     RAM_usage_before=RAM_usage
+ else:
+     time.sleep(1)
